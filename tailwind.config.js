039f6b763074d19559a46e25d56158e1885c2adb
rsourdis <46548082+rsourdis@users.noreply.github.com>
2022-04-29 13:33:04 +0200

    Adding extra config ts and tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,28 @@
 module.exports = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx}',
-    './src/components/**/*.{js,ts,jsx,tsx}',
-    './src/containers/**/*.{js,ts,jsx,tsx}',
-  ],
+  content: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}', './src/containers/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    // fontFamily: {
+    //   openSansRegular: 'openSansRegular',
+    //   openSansLight: 'openSansLight',
+    //   openSansMedium: 'openSansMedium',
+    //   openSansSemiBold: 'openSansSemiBold',
+    //   openSansBold: 'openSansBold',
+    //   openSansExtraBold: 'openSansExtraBold',
+    //   quicoRegular: 'quicoRegular',
+    //   quicoExtraLight: 'quicoExtraLight',
+    //   quicoMedium: 'quicoMedium',
+    //   quicoExtraBold: 'quicoExtraBold',
+    //   quicoSemiBold: 'quicoSemiBold',
+    // },
+    extend: {
+      colors: {
+        // 'qnco-morado': '#6463B1',
+        // 'qnco-morado-oscuro': '#323259',
+        // 'qnco-amarillo': '#FFD200',
+        // 'qnco-verde': '#21D5B1',
+        // 'qnco-naranja': '#FF6C00',
+      },
+    },
   },
   plugins: [],
-}
+};
---
+++ typings.d.ts
@@ -0,0 +1,47 @@
+// interface Image {
+//     asset: {
+//       url: string;
+//     };
+//   }
+  
+//   export interface Cuenco {
+//     _id: string;
+//     title: string;
+//     slug: {
+//       current: string;
+//     };
+//     price: number;
+//     description: string;
+//     listIngredients: string;
+//     listAlergens: string;
+//     tags: [];
+//     categorias: [];
+//     image: Image;
+//     ingredientes: Ingredientes[];
+//   }
+  
+//   export interface Extra {
+//     _id: string;
+//     title: string;
+//     slug: {
+//       current: string;
+//     };
+//     price: number;
+//     description: string;
+//     tags: [];
+//     categorias: [];
+//     image: Image;
+//   }
+  
+//   export interface Ingredientes {
+//     _id: string;
+//     title: string;
+//     slug: {
+//       current: string;
+//     };
+//     costoTotal: number;
+//     costoUnidad: number;
+//     measure: number;
+//     measureType: string;
+//   }
+  
